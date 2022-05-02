27dea3de278f3c2a1ffb0676c0c9c3722dabd75d
xxx <humengmingxx@gmail.com>
2022-05-02 17:53:44 +0800

    feat(single-line-comment)：done

--- playground/index.ts
+++ playground/index.ts
@@ -2,7 +2,7 @@ const xxx = 'xxx'
 
 export const TestFn = () => {
   const xxx = ''
-  console.warn('xxx#xxx', xxx)
+  console.warn('xxx#xx', xxx)
 }
 
 console.warn('xxx#xxx', xxx)
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,10 @@
 import { Selection, commands, window } from 'vscode'
 import type { ExtensionContext, TextEditor } from 'vscode'
 
-/**
- *
- */
-// const getTabSize = () => {
-//   return ''
-// }
+const MAX_WIDTH = 40
+const START_SYMBOL = '/'
+const END_SYMBOL = '/'
+const FILL_SYMBOL = '*'
 
 /**
  * 获取当前选中的文本
@@ -18,52 +16,79 @@ const getSelectedText = (editor: TextEditor) => {
   const last = document.lineAt(endLine).range.end.character
   const newSelection = new Selection(startLine, 0, endLine, last)
 
-  return editor.document.getText(newSelection)
+  return ({
+    selection: newSelection,
+    text: document.getText(newSelection),
+  })
 }
 
 /**
- * 获取语言
+ * 是否是整数
  */
-const getLanguage = (editor: TextEditor) => {
-  return editor?.document?.languageId
+const isInteger = (num: number) => {
+  return num % 1 === 0
+}
+
+/**
+ * 创建重复字符
+ */
+const createRepeatChars = (chars: string, num: number): string => {
+  return chars.repeat(num)
 }
 
 /**
  * 生成注释
  */
 const generateComments = (text: string) => {
+  if (!text)
+    return ''
+
   const textArr = text
     .split(/\n/)
     .filter(v => v.match(/\S/))
-    .map(s => s.replace(/\s*$/, ''))
-  // TODO:
-  return textArr
+    .map(s => s.replace(/(^\s*)|(\s*$)/, ''))
+
+  const maxLineWidth = textArr.reduce((max, str) => Math.max(max, str.length), 0)
+  const fillWidth = (MAX_WIDTH - maxLineWidth - 2) / 2
+
+  const firstLine = `${START_SYMBOL}${createRepeatChars(FILL_SYMBOL, MAX_WIDTH - 1)}`
+  const middleLine = `${FILL_SYMBOL}${createRepeatChars(' ', MAX_WIDTH - 2)}${FILL_SYMBOL}`
+  const endLine = `${createRepeatChars(FILL_SYMBOL, MAX_WIDTH - 1)}${END_SYMBOL}`
+  const leftFilled = isInteger(fillWidth) ? Math.floor(fillWidth) : Math.ceil(fillWidth)
+  const rightFilled = fillWidth
+  const middleText = `${FILL_SYMBOL}${createRepeatChars(' ', leftFilled)}${textArr[0]}${createRepeatChars(' ', rightFilled)}${FILL_SYMBOL}`
+
+  return `${firstLine}\n${middleLine}\n${middleText}\n${middleLine}\n${endLine}`
 }
 
 export function activate(context: ExtensionContext) {
   /**
-   * 实现步骤：
-   *    1、获取 当前选中的文本
-   *    2、获取 当前 语言标识
-   *    3、计算 注释长度
-   *        a、当前语言支持的注释格式
-   *        b、求最合适的长度
+   * 第一版：完成最小维度的闭环
+   *
+   * TODO：
+   *    1、支持多行注释
+   *    2、支持多语言
+   *    3、支持自定义配置
+   *        a、自定义符号
+   *        b、自定义宽度
+   *    4、锚点定位???
+   *
+   * ISSUES：
+   *    1、中文注释长度无法确定
+   *    2、整体注释缩进问题
+   *
    */
   const startWorking = () => {
     const editor = window.activeTextEditor
+    const { text, selection } = getSelectedText(editor!)
+    const commentsText = generateComments(text)
 
-    const selectedText = getSelectedText(editor!)
-    const commentsText = generateComments(selectedText)
-
-    const currentLanguage = getLanguage(editor!)
-
-    console.warn('xxx#selectedText\n', selectedText)
-    console.warn('------------------------------------------------\n')
-    console.warn('xxx#commentsText\n', commentsText)
-    console.warn('xxx#currentLanguage', currentLanguage)
+    editor?.edit((builder) => {
+      builder.replace(selection, commentsText)
+    })
   }
 
-  context.subscriptions.push(commands.registerCommand('layered-comments.xxx', startWorking))
+  context.subscriptions.push(commands.registerCommand('layered-comments.create', startWorking))
 }
 
 export function deactivate() { }
