a301acc94382e7cca80faec6ec01cf7cb7fa424a
xxx <humengmingxx@gmail.com>
2022-05-03 21:54:28 +0800

    feat(playground)：add comments

--- playground/index.ts
+++ playground/index.ts
@@ -1,8 +1,5 @@
-const xxx = 'xxx'
-
-export const TestFn = () => {
-  const xxx = ''
-  console.warn('xxx#xx', xxx)
-}
-
-console.warn('xxx#xxx', xxx)
+/***************************************
+*                                      *
+*                  xxx                 *
+*                                      *
+***************************************/
